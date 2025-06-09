0a8d33e475320c24babe6eda378b8516d90a85d7
gklodfox <gklodkowski@gmail.com>
2025-06-09 23:21:42 +0200

    ai

--- lua/fox-neovim-config/keymaps.lua
+++ lua/fox-neovim-config/keymaps.lua
@@ -19,102 +19,106 @@ vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
 
 vim.keymap.set("n", "<leader>a", function()
-  require("harpoon"):list():add()
+    require("harpoon"):list():add()
 end)
 vim.keymap.set("n", "<C-e>", function()
-  require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+    require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
 end)
 
 vim.keymap.set("n", "<C-h>", function()
-  require("harpoon"):list():select(1)
+    require("harpoon"):list():select(1)
 end)
 vim.keymap.set("n", "<C-t>", function()
-  require("harpoon"):list():select(2)
+    require("harpoon"):list():select(2)
 end)
 vim.keymap.set("n", "<C-n>", function()
-  require("harpoon"):list():select(3)
+    require("harpoon"):list():select(3)
 end)
 vim.keymap.set("n", "<C-s>", function()
-  require("harpoon"):list():select(4)
+    require("harpoon"):list():select(4)
 end)
 
 -- Toggle previous & next buffers stored within require('harpoon') list
 vim.keymap.set("n", "<C-S-P>", function()
-  require("harpoon"):list():prev()
+    require("harpoon"):list():prev()
 end)
 vim.keymap.set("n", "<C-S-N>", function()
-  require("harpoon"):list():next()
+    require("harpoon"):list():next()
 end)
 
 require("which-key").add({
-  mode = { "n", "v" },
-  cond = false, -- function()
-  --   if pcall(require, "chatgpt") then
-  --     return true
-  --   end
-  --   return false
-  -- end,
-  { "<leader>pc", "<cmd>ChatGPT<CR>", desc = "ChatGPT", icon = "󱙺" },
-  { "<leader>pe", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
-  { "<leader>pg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
-  { "<leader>pt", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
-  { "<leader>pk", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
-  { "<leader>pd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
-  { "<leader>pa", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
-  { "<leader>po", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
-  { "<leader>ps", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
-  { "<leader>pf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
-  { "<leader>px", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
-  { "<leader>pr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
-  { "<leader>pl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
+    mode = { "n", "v" },
+    cond = function()
+        if pcall(require, "chatgpt") then
+            return true
+        end
+        return false
+    end,
+    { "<leader>cc", "<cmd>ChatGPT<CR>", desc = "ChatGPT", icon = "󱙺" },
+    { "<leader>ce", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+    { "<leader>cg", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+    { "<leader>ct", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+    { "<leader>ck", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+    { "<leader>cd", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+    { "<leader>ca", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+    { "<leader>co", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+    { "<leader>cs", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+    { "<leader>cf", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+    { "<leader>cx", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
+    { "<leader>cr", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+    { "<leader>cl", "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
 })
 
 require("which-key").add({
-  mode = { 'n' },
-  cond = function()
-      if pcall(require, "ufo") then
-          return true
-      end
-      return false
-  end,
-  { "<leader>zR", require("ufo").openAllFolds, desc = "Open all folds under the cursor position" },
-  { "<leader>zM", require("ufo").closeAllFolds, desc = "Close all folds under the cursor position" },
-  { "<leader>zr", require("ufo").openFoldsExceptKinds, desc = "Open folds under the cursor, except for kinds"},
-  { "<leader>zm", require("ufo").closeFoldsWith, desc = "Close folds under the cursor with..."}
+    mode = { "n" },
+    cond = function()
+        if pcall(require, "ufo") then
+            return true
+        end
+        return false
+    end,
+    { "<leader>zR", require("ufo").openAllFolds, desc = "Open all folds under the cursor position" },
+    { "<leader>zM", require("ufo").closeAllFolds, desc = "Close all folds under the cursor position" },
+    { "<leader>zr", require("ufo").openFoldsExceptKinds, desc = "Open folds under the cursor, except for kinds" },
+    { "<leader>zm", require("ufo").closeFoldsWith, desc = "Close folds under the cursor with..." },
 })
 
 -- fzf-lua
 require("which-key").add({
-  mode = { "n" },
-  cond = function()
-    if pcall(require, "fzf-lua") then
-      return true
-    end
-    return false
-  end,
-  { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find files in CWD" },
-  { "<leader>fF", "<cmd>FzfLua git_files<CR>", desc = "Find files in git CWD" },
-  { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Grep files in CWD" },
-  { "<leader>fG", "<cmd>FzfLua live_grep cmd = 'git grep --line-number --column --color=always'<CR>", desc = "Grep files in git CWD" },
-  { "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Find buffer" },
-  { "<leader>fk", "<cmd>FzfLua keymaps<CR>", desc = "Find keymaps" },
-  { "<leader>fH", "<cmd>FzfLua highlights<CR>", desc = "Find highlights" },
-  { "<leader>fh", "<cmd>FzfLua help_tags<CR>", desc = "Find help tags" },
-  { "<leader>fc", "<cmd>FzfLua command_history<CR>", desc = "Search command history" },
-  { "<leader>fo", "<cmd>FzfLua vim_options<CR>", desc = "Lookup values of neovim settings" },
-  { "<leader>fo", "<cmd>FzfLua diagnostics_workspace<CR>", desc = "Search through workspace diagnostics" },
-  { "<leader>fp", "<cmd>NeovimProjectDiscover<CR>", desc = "Find project" },
+    mode = { "n" },
+    cond = function()
+        if pcall(require, "fzf-lua") then
+            return true
+        end
+        return false
+    end,
+    { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find files in CWD" },
+    { "<leader>fF", "<cmd>FzfLua git_files<CR>", desc = "Find files in git CWD" },
+    { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Grep files in CWD" },
+    {
+        "<leader>fG",
+        "<cmd>FzfLua live_grep cmd = 'git grep --line-number --column --color=always'<CR>",
+        desc = "Grep files in git CWD",
+    },
+    { "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Find buffer" },
+    { "<leader>fk", "<cmd>FzfLua keymaps<CR>", desc = "Find keymaps" },
+    { "<leader>fH", "<cmd>FzfLua highlights<CR>", desc = "Find highlights" },
+    { "<leader>fh", "<cmd>FzfLua help_tags<CR>", desc = "Find help tags" },
+    { "<leader>fc", "<cmd>FzfLua command_history<CR>", desc = "Search command history" },
+    { "<leader>fo", "<cmd>FzfLua vim_options<CR>", desc = "Lookup values of neovim settings" },
+    { "<leader>fo", "<cmd>FzfLua diagnostics_workspace<CR>", desc = "Search through workspace diagnostics" },
+    { "<leader>fp", "<cmd>NeovimProjectDiscover<CR>", desc = "Find project" },
 })
 
 -- nvim-lualine/lualine.nvim
 require("which-key").add({
-  mode = { "n" },
-  cond = function()
-    if pcall(require, "lualine") then
-      return true
-    end
-    return false
-  end,
-  { "gb", "<cmd>LualineBuffersJump! " ..tostring(vim.v.count).. "<CR>", desc = "Go to buffer" },
-  { "gB", '<cmd>LualineBuffersJump! $<CR>', desc = "Go to last buffer" },
+    mode = { "n" },
+    cond = function()
+        if pcall(require, "lualine") then
+            return true
+        end
+        return false
+    end,
+    { "gb", "<cmd>LualineBuffersJump! " .. tostring(vim.v.count) .. "<CR>", desc = "Go to buffer" },
+    { "gB", "<cmd>LualineBuffersJump! $<CR>", desc = "Go to last buffer" },
 })
---
+++ lua/fox-neovim-config/plugins/chatgpt.lua
@@ -0,0 +1,30 @@
+local M = { "jackMort/ChatGPT.nvim" }
+
+M.dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim", -- optional
+    "nvim-telescope/telescope.nvim",
+}
+
+M.event = "VeryLazy"
+function M.opts()
+    return {
+        api_key_cmd = "pass private/openai/api/token",
+        openai_params = {
+            model = "gpt-4o",
+            frequency_penalty = 0,
+            presence_penalty = 0,
+            max_tokens = 4095,
+            temperature = 0.2,
+            top_p = 0.1,
+            n = 1,
+        },
+    }
+end
+
+function M.config(_, opts)
+    require("chatgpt").setup(opts)
+end
+
+return M
