998de76503b16773eea21c1208ab5b3dd568f9dc
gklodfox <gklodkowski@gmail.com>
2025-06-21 21:56:25 +0200

    test

--- lua/fox-neovim-config/plugins/convenience/init.lua
+++ lua/fox-neovim-config/plugins/convenience/init.lua
@@ -1,4 +1,4 @@
 return {
     require('fox-neovim-config.plugins.convenience.which-key'),
-    require('fox-neovim-config.plugins.convenience.telescope-nvim')
+    -- require('fox-neovim-config.plugins.convenience.telescope-nvim')
 }
