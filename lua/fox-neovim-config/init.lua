bc7a44f3648587928f7425642ba89be17dac71ea
gklodfox <gklodkowski@gmail.com>
2025-06-21 03:35:58 +0200

    fixes

--- lua/fox-neovim-config/init.lua
+++ lua/fox-neovim-config/init.lua
@@ -1,4 +1,6 @@
-require('fox-neovim-config.core.options')
-require('fox-neovim-config.core.keymaps')
-require('fox-neovim-config.core.autocommands')
-require('fox-neovim-config.core.lazy')
+require('fox-neovim-config.core')
+
+require('fox-neovim-config.options')
+require('fox-neovim-config.keymaps')
+require('fox-neovim-config.autocommands')
+require('fox-neovim-config.lazy')
