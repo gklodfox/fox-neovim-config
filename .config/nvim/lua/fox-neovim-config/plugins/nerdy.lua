d76a768ecbfead51df46fceeb28fa5b461edb6e0
gklodfox <gklodkowski@gmail.com>
2025-09-07 09:43:30 +0200

    sync

--- .config/nvim/lua/fox-neovim-config/plugins/nerdy.lua
+++ .config/nvim/lua/fox-neovim-config/plugins/nerdy.lua
@@ -1,8 +1,13 @@
 local M = {'2kabhishek/nerdy.nvim'}
-M.dependencies = {
-    'stevearc/dressing.nvim',
-}
--- M.enabled = false
+M.dependencies = { 'folke/snacks.nvim' }
 M.cmd = 'Nerdy'
+
+function M.opts()
+    return {
+        max_recents = 30,
+        add_default_keybindngs = true,
+        copy_to_clipboard = false,
+    }
+end
 
 return M
