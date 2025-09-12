d28ae2bee5ec5a2c60f5bf0fdaf2822d33e83a3a
gklodfox <gklodkowski@gmail.com>
2025-09-12 06:39:06 +0200

    Changes to be committed: 	new file:   .config/nvim/lua/fox-neovim-config/plugins/avante.lua

---
+++ .config/nvim/lua/fox-neovim-config/plugins/avante.lua
@@ -0,0 +1,68 @@
+local M = { 'yetone/avante.nvim' }
+
+M.enabled = false
+
+M.dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "stevearc/dressing.nvim", -- for input provider dressing
+    "folke/snacks.nvim", -- for input provider snacks
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+}
+
+M.build = 'make'
+M.event = 'VeryLazy'
+M.version = false
+
+function M.opts()
+    return {
+        instructions_file = '.ai.md',
+        provider = 'openai',
+        mode = 'agentic',
+        auto_suggestions_provider = 'openai',
+        providers = {
+            openai = {
+                endpoint = nil,
+                model = nil,
+                timeout = 30000,
+                extra_request_body = {
+                    temperature = 0.75,
+                    max_tokens = 20480,
+                }
+            }
+        }
+    }
+end
+
+return M
