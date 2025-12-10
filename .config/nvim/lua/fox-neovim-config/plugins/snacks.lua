d9a43f94c27be833b7cf4ee1f63dc43add659d63
gklodfox <gklodkowski@gmail.com>
2025-12-10 04:22:49 +0100

    sync

--- .config/nvim/lua/fox-neovim-config/plugins/snacks.lua
+++ .config/nvim/lua/fox-neovim-config/plugins/snacks.lua
@@ -25,6 +25,13 @@ function M.init()
             Snacks.dashboard()
         end
     })
+    vim.api.nvim_set_hl(0, "RainbowRed", {fg = "#E06C75"})
+    vim.api.nvim_set_hl(0, "RainbowYellow", {fg = "#E5C07B"})
+    vim.api.nvim_set_hl(0, "RainbowBlue", {fg = "#61AFEF"})
+    vim.api.nvim_set_hl(0, "RainbowOrange", {fg = "#D19A66"})
+    vim.api.nvim_set_hl(0, "RainbowGreen", {fg = "#98C379"})
+    vim.api.nvim_set_hl(0, "RainbowViolet", {fg = "#C678DD"})
+    vim.api.nvim_set_hl(0, "RainbowCyan", {fg = "#56B6C2"})
 end
 
 function M.opts()
